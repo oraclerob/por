7f15c44d6643b6a997df96c1fce05bbb3cb2361e
oraclerob <rmascaro@scnet.com.au>
2020-05-01 14:05:34 +0800

    Fix dummy GPIO

--- RPi/GPIO.py
+++ RPi/GPIO.py
@@ -3,13 +3,14 @@ OUT = 1
 IN = 1
 BCM = 1
 PUD_UP = 1
+PUD_DOWN = 0
 HIGH = 1
 LOW = 0
 
 def setmode(a):
     print(a)
 
-def setup(a, b):
+def setup(a, b, pull_up_down):
     print (a)
 
 def output(a, b):
@@ -19,6 +20,9 @@ def cleanup():
     print ('a')
 
 def setmode(a):
+    print (a)
+
+def input(a):
     print (a)
 
 def setwarnings(flag):
